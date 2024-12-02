5c11a06c2f1c4e213c3ae850fdc589a9ce8a2e50
Khanya <Kaymvokoalo@icloud.com>
2024-12-02 10:43:15 +0200

    Initial commit

--- conf.py
+++ conf.py
@@ -1,7 +1,10 @@
 import os
 import sys
+import django
 
-
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'MYSITE.settings'
+django.setup()
 
 
 # Configuration file for the Sphinx documentation builder.
